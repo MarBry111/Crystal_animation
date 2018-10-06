9f589b8c686ac1eaec7256dd6c1d981ed17d3a16
Marek Brynda <mrmareczek111@gmail.com>
2018-10-06 14:57:39 +0200

    cute plot

--- main.py
+++ main.py
@@ -17,20 +17,10 @@ T_0 = 100     # Temperature
 k = 0.00831 # Boltzman constant
 save_to_file = False
 
+
 def e_kin_ax(T_0):
     lam = np.random.random()
     return -1 / 2 * k * T_0 * np.log(lam)
-
-'''
-def momentum_ax(E_kin, m):
-    temp = random.random()
-    char = 0
-    if temp > 0.5:
-        char = 1
-    else:
-        char = -1
-    return char * np.sqrt(2 * m * E_kin)
-'''
 
 
 def momentum_ax(T_0, m):
@@ -43,6 +33,23 @@ def momentum_ax(T_0, m):
     return char * np.sqrt(2 * m * e_kin_ax(T_0))
 
 
+def force_P(epsilon, R, r_arr, index):
+    r_i = r_arr[index]
+    forces_P = []
+    for ii in N-1:
+        if r_arr[ii] != r_i:
+            r_j = r_arr[ii]
+            r_ij = np.abs(r_i-r_j)
+            forces_P = np.append(12*epsilon*((R/r_ij)**12-(R/r_ij)**6)*(r_i-r_j)/r_ij**2)
+    return np.sum(forces_P)
+
+
+def force_s(r_i, L, f):
+    if r_i < L:
+        return 0
+    elif r_i >= L:
+        return f*(L - r_i)
+
 # vectors showing edges of cell
 b_0 = np.array([a, 0, 0])
 b_1 = np.array([a / 2, a * np.sqrt(3) / 2, 0])
@@ -53,43 +60,32 @@ i = range(n)
 i = np.array(i)
 
 # coordinates
-r_i = []
+r_arr = []
 for j in range(n):
     for jj in range(n):
         for jjj in range(n):
             x = b_0[0] * (i[j] - (n - 1) / 2) + b_1[0] * (i[jj] - (n - 1) / 2) + b_2[0] * (i[jjj] - (n - 1) / 2)
             y = b_0[1] * (i[j] - (n - 1) / 2) + b_1[1] * (i[jj] - (n - 1) / 2) + b_2[1] * (i[jjj] - (n - 1) / 2)
             z = b_0[2] * (i[j] - (n - 1) / 2) + b_1[2] * (i[jj] - (n - 1) / 2) + b_2[2] * (i[jjj] - (n - 1) / 2)
-            r_i.append([x, y, z])
-r_i = np.array(r_i)
-
-'''
-# energie kinetyczne
-E_kin_i = []
-for j in range(n):
-    for jj in range(n):
-        for jjj in range(n):
-            E_kin_x = e_kin_ax(T_0)
-            E_kin_y = e_kin_ax(T_0)
-            E_kin_z = e_kin_ax(T_0)
-            E_kin_i.append([E_kin_x, E_kin_y, E_kin_z])
-E_kin_i = np.array(E_kin_i)
-'''
+            r_arr.append([x, y, z])
+r_arr = np.array(r_arr)
 
 # momentum
-p_i = []
+p_arr = []
 for j in range(N):
     p_x = momentum_ax(T_0, m)
     p_y = momentum_ax(T_0, m)
     p_z = momentum_ax(T_0, m)
-    p_i.append([p_x, p_y, p_z])
-p_i = np.array(p_i)
+    p_arr.append([p_x, p_y, p_z])
+p_arr = np.array(p_arr)
 
 # normalize momentum
-P = np.array([np.sum(p_i[:, 0]), np.sum(p_i[:, 1]), np.sum(p_i[:, 2])])/N
-p_i[:] = p_i[:] - P
+P = np.array([np.sum(p_arr[:, 0]), np.sum(p_arr[:, 1]), np.sum(p_arr[:, 2])])/N
+p_arr[:] = p_arr[:] - P
 
-#dopisac sily
+#forces
+force_arr = []
+
 
 if save_to_file:
     delimiter = " "
@@ -109,9 +105,9 @@ if save_to_file:
 #ax.scatter3D(p_i[:,0], p_i[:,1], p_i[:,2])
 #ax.set_ylim3d(ax.get_xlim3d())
 #ax.set_zlim3d(ax.get_xlim3d())
-ay, ax = np.histogram(np.abs(p_i[:,0]), 30)
-by, bx = np.histogram(np.abs(p_i[:,1]), 30)
-cy, cx = np.histogram(np.abs(p_i[:,2]), 30)
+ay, ax = np.histogram(np.abs(p_arr[:,0]), 30)
+by, bx = np.histogram(np.abs(p_arr[:,1]), 30)
+cy, cx = np.histogram(np.abs(p_arr[:,2]), 30)
 plt.plot(.5*(ax[1:]+ax[:-1]), ay)
 plt.plot(.5*(bx[1:]+bx[:-1]), by)
 plt.plot(.5*(cx[1:]+cx[:-1]), cy)
